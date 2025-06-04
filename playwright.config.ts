e1b55de49feb084bd2fba4b1058f02fd7613f6c6
Luis Alberto P.C. <luisalbertodev@gmail.com>
2025-06-04 17:29:10 -0600

    chore: :wrench: add playwright

---
+++ playwright.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests/e2e',
+  timeout: 30 * 1000,
+  retries: 0,
+  projects: [
+    {
+      name: 'Chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'Firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'WebKit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+  use: {
+    headless: true,
+    baseURL: 'http://localhost:6006',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
+});
